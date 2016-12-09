a8a8fb66fe80133ce27b9e45209c457a22296fed
Daithi Coombes <webeire@gmail.com>
2016-12-09 19:53:15 +0000

    concept acceptance tests, update mockup

---
+++ tests/acceptance/list-days-test.js
@@ -0,0 +1,16 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'tech-test/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | list days');
+
+test('should show graphic data for current city', function(assert){
+});
+
+test('should show date and textual data', function(assert){
+});
+
+test('should list 16 days after today, with basic info', function(assert){
+});
+
+test('day tile should link to single day', function(assert){
+});
---
+++ tests/acceptance/single-day-test.js
@@ -0,0 +1,10 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'tech-test/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | single day');
+
+test('back button should link to relevant list days', function(assert){
+});
+
+test('should show detailed information', function(assert){
+});
